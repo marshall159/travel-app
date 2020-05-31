93f7ec380f17adc65badc37f084e3d7131d923e1
Aneel Marshall <aneelmarshall@outlook.com>
2020-05-31 17:07:34 +0100

    feat: add route to fetch data from weatherbit

--- src/server/index.js
+++ src/server/index.js
@@ -5,7 +5,9 @@ const dotenv = require('dotenv');
 dotenv.config();
 const PORT = process.env.PORT || 8082;
 const cors = require('cors');
-const bodyParser = require('body-parser')
+const bodyParser = require('body-parser');
+const fetch = require('node-fetch');
+const WEATHERBIT_API = process.env.WEATHERBIT_API || '';
 
 const app = express();
 
@@ -20,6 +22,22 @@ app.get('/', function (req, res) {
 app.post('/', function (req, res) {
     console.log('Request to POST /');
     res.json(mockAPIResponse);
+});
+
+app.get('/weather/:lat/:lon', async (req, res) => {
+    const { lon, lat } = req.params;
+    
+    const BASE_API_PATH = `https://api.weatherbit.io/v2.0/forecast/daily`;
+    const query = `?&lat=${lat}&lon=${lon}`;
+    const url = BASE_API_PATH + query + `&key=${WEATHERBIT_API}`;
+
+    try {
+        const response = await fetch(url);
+        const json = await response.json();
+        return res.json(json);
+    } catch (err) {
+        console.error(`Error: ${err.message}`);
+    }
 });
 
 app.listen(PORT, function () {
